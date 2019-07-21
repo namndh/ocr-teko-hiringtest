44ec5e3711a11601bccac0edc7bf11c5cbaef102
t3min4l <hainam.nguyen.ict@gmail.com>
2019-07-21 23:58:37 +0700

    update

--- pre-processing.py
+++ pre-processing.py
@@ -4,14 +4,14 @@ import cv2
 import glob
 import numpy as np
 import matplotlib.pyplot as plt
-from utils import *
+from util import *
 
 import torch 
 from PIL import Image
 from torch.autograd import Variable
 from crnn_pytorch import utils
+import crnn_pytorch.models.crnn as crnn
 from crnn_pytorch import dataset
-from crnn_pytorch import models
 
 def display_img(img):
 	cv2.imshow('img', img)
@@ -32,70 +32,111 @@ def equalize(list1, list2):
 	else:
 		return False
 
-img_search_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "*.jpg")
-img_path = glob.glob(img_search_path)[0]
-print(img_path)
+def load_crnn():
+	model_path = "./crnn_pytorch/data/crnn.pth"
+	alphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
+	model = crnn.CRNN(32, 1, 37, 256)
+
+	if torch.cuda.is_available():
+		model = model.cuda()
+	print("Loading CRNN...")
+	model.load_state_dict(torch.load(model_path))
+	converter = utils.strLabelConverter(alphabet)
+	transformer = dataset.resizeNormalize((100, 32))
+	model.eval()
+	return model, converter, transformer
+
+def word_recognition(words, words_imgs):
+	for word in words_imgs:
+		word_pil = Image.fromarray(word).convert('L')
+		word = transformer(word_pil)
+		# word = Variable(word)
+		if torch.cuda.is_available():
+			word = word.cuda()
+		word = word.view(1, *word.size())
+		word = Variable(word)
+		
+		preds = model(word)
+		_, preds = preds.max(2)
+		preds = preds.transpose(1, 0).contiguous().view(-1)
+		preds_size = Variable(torch.IntTensor([preds.size(0)]))
+		sim_pred = converter.decode(preds.data, preds_size.data, raw=False)
+		words.append(sim_pred + " ")
+
+def extract_lines(img):
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	img1 = img.copy()
+
+	ret, thresh = cv2.threshold(img, 60, 240, cv2.THRESH_BINARY_INV)
+	pts = cv2.findNonZero(thresh)
+	box = cv2.minAreaRect(pts)
+
+	(cx, cy), (w, h), angle = box
+	if w > h:
+		w, h = h, w
+		angle += 90
+	M = cv2.getRotationMatrix2D((cx, cy), angle, 1.0)
+	rotated = cv2.warpAffine(thresh, M, (img.shape[1], img.shape[0]))
+
+	hist = cv2.reduce(rotated, 1, cv2.REDUCE_MAX).reshape(-1)
+	th = 10
+	H, W = img.shape[:2]
+	uppers = [y for y in range(H-1) if hist[y] <= th and hist[y+1] > th]
+	lowers = [y for y in range(H-1) if hist[y] > th and hist[y+1] <= th]
+	print(len(uppers), len(lowers))
+	rotated = cv2.cvtColor(rotated, cv2.COLOR_GRAY2BGR)
+
+	for y in uppers:
+		cv2.line(rotated, (0, y), (W, y), (255, 0, 0), 1)
+
+	for y in lowers:
+		cv2.line(rotated, (0, y), (W, y), (0, 255, 0), 1)
+
+	lines = list()
+	print(len(uppers), len(lowers))
+	for idx, (yupper, ylower) in enumerate(zip(uppers, lowers)):
+		# cv2.imwrite(os.path.join(os.path.abspath(os.path.dirname(__file__)), str(idx)+".png") ,img1.copy()[yupper-1:ylower+7,:])
+		lines.append(cv2.cvtColor(
+			img1.copy()[yupper-1:ylower+7, :], cv2.COLOR_GRAY2RGB))
+	
+	return lines	
 
 black = [10,10,10]
 white = [255,255,255]
 
-img = cv2.imread(img_path)
+img = cv2.imread("./20000-leagues-006.jpg")
 
 img = img[:,:int(img.shape[1]*0.5),:]
 
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img1 = img.copy()
-	
-ret, thresh = cv2.threshold(img, 60, 240, cv2.THRESH_BINARY_INV)
-pts = cv2.findNonZero(thresh)
-box = cv2.minAreaRect(pts)
 
-(cx,cy), (w,h), angle = box 
-if w>h:
-	w,h = h,w
-	angle += 90
-M = cv2.getRotationMatrix2D((cx,cy), angle, 1.0)
-rotated = cv2.warpAffine(thresh, M, (img.shape[1], img.shape[0]))
 
-hist = cv2.reduce(rotated,1, cv2.REDUCE_MAX).reshape(-1)
-th = 10
-H,W = img.shape[:2]
-uppers = [y for y in range(H-1) if hist[y]<=th and hist[y+1]>th]
-lowers = [y for y in range(H-1) if hist[y]>th and hist[y+1]<=th]
-print(len(uppers), len(lowers))
-rotated = cv2.cvtColor(rotated, cv2.COLOR_GRAY2BGR)
+lines = extract_lines(img)
+print(len(lines))
 
-for y in uppers:
-    cv2.line(rotated, (0,y), (W, y), (255,0,0), 1)
 
-for y in lowers:
-    cv2.line(rotated, (0,y), (W, y), (0,255,0), 1)
 
-lines = list()
-print(len(uppers), len(lowers))
-for idx,(yupper, ylower) in enumerate(zip(uppers, lowers)):
-	cv2.imwrite(os.path.join(os.path.abspath(os.path.dirname(__file__)), str(idx)+".png") ,img1.copy()[yupper-1:ylower+7,:])
-	# lines.append(cv2.cvtColor(img1.copy()[yupper-1:ylower+7,:], cv2.COLOR_BGR2RGB))
+words_p1 = list()
+model, converter, transformer = load_crnn()
 
-# model_path = "./crnn_pytorch/data/crnn.pth"
-# alphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
-# model = models.crnn.CRNN(31,1,37,256)
-# if torch.cuda.is_available():
-# 	model = models.cuda()
-# print("Loading CRNN...")
-# model.load_state_dict(torch.load(model_path))
- 
-# converter = utils.strLabelConverter(alphabet)
-# transformer = dataset.resizeNormalize((100,32))
-# for idx, line in enumerate(lines):
-# 	words = segment_words(line)
-# 	for word in words:
-# 		word_pil = Image.fromarray(word)
-# 		word = transformer(word_pil)
-# 		# word = Variable(word)
-# 		if torch.cuda.is_available():
-# 			word = word.cuda()
-# 		word = word.view(1, *word.size())
-# 		image = Variable(image)
+for idx, line in enumerate(lines):
+	words_imgs = segment_words(line)
+	word_recognition(words_p1, words_imgs)
+	words_p1.append('\n')
+
+
+
+print("Page 1:\n\n")
+print(''.join(words_p1)+'\n\n')
+print("Page 2:\n\n")
+
+img2 = cv2.imread("./20000-leagues-006.jpg")
+img2 = img2[:, int(img2.shape[1]*0.5):, :]
+# display_image(img2)
+words = segment_words(img2)
+del words[0]
+words_p2 = list()
+
+word_recognition(words_p2, words)
+print(''.join(words_p2))
 
 
--- util.py
+++ util.py
@@ -2,9 +2,9 @@ import os
 import sys
 import cv2
 import numpy as np
-import glob
-img_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '18.png')
-img_name = '18'
+
+img_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '13.png')
+img_name = '13'
 def display_image(img):
 	cv2.imshow('img', img)
 	cv2.waitKey()
@@ -27,7 +27,6 @@ def find_bounding_box(img):
     return cnts
 
 def segment_words(line):
-	line_rgb = line.copy()
 	if len(line.shape) >= 3:
 		line_gray = cv2.cvtColor(line, cv2.COLOR_BGR2GRAY)
 		ret, line = cv2.threshold(line_gray, 75, 240, cv2.THRESH_BINARY)
@@ -38,33 +37,14 @@ def segment_words(line):
 	res = find_bounding_box(line_cp1)
 	for (c, _) in res:
 		(x,y,w,h) = cv2.boundingRect(c)
-		if w >= 5 and h >= 15:
+		if w >= 7 and h >= 15:
 			cv2.rectangle(line_cp1, (x,y), (x+w,y+h), (0,0,0),3)
 	res1 = find_bounding_box(line_cp1)
 	words = list()
 	for (c,_) in res1:
 		(x,y,w,h) = cv2.boundingRect(c)
 		if w >= 15 and h >= 20:
-			words.append(line_rgb[y:y+h, x:x+w])
-			display_image(line_rgb[y:y+h, x:x+w])
-			cv2.imwrite('word.png', line[y:y+h, x:x+w])
+			words.append(line_cp3[y:y+h, x:x+w])
 			cv2.rectangle(line_cp2, (x,y), (x+w, y+h), (0,0,0), 1)
 	# display_image(line_cp2)
-	# cv2.imwrite('page2.png', line_cp2)
 	return words
-
-
-img_search_path = os.path.join(
-	os.path.abspath(os.path.dirname(__file__)), "*.jpg")
-img_path = glob.glob(img_search_path)[0]
-print(img_path)
-
-black = [10, 10, 10]
-white = [255, 255, 255]
-
-img = cv2.imread("20000-leagues-006.jpg")
-
-img = img[:, int(img.shape[1]*0.5):, :]
-display_image(img)
-lines = segment_words(img)
-print(len(lines))
