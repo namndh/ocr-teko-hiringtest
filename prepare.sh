382ba81c62753dd162ab55888f568194ed4b3d1d
t3min4l <hainam.nguyen.ict@gmail.com>
2019-07-22 23:27:59 +0700

    add shell script file

---
+++ prepare.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone https://github.com/meijieru/crnn.pytorch.git
+mv crnn.pytorch crnn_pytorch
+ls
+wget https://www.dropbox.com/s/dboqjk20qjkpta3/crnn.pth -P crnn_pytorch/data/
+
