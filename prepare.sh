0031b6f8f59ab282cf05302f0e97b2c48498f36a
namndh <namnd@aimesoft.com>
2019-07-24 14:50:03 +0700

    update

--- prepare.sh
+++ prepare.sh
@@ -3,4 +3,4 @@ git clone https://github.com/meijieru/crnn.pytorch.git
 mv crnn.pytorch crnn_pytorch
 ls
 wget https://www.dropbox.com/s/dboqjk20qjkpta3/crnn.pth -P crnn_pytorch/data/
-wget wget --no-check-certificate https://docs.google.com/uc\?export\=download\&id\=1hmtbUQC5HuLb1KOMozNwCKFoAPa56rtx -P prediction_model.hdf5
+wget wget --no-check-certificate https://docs.google.com/uc\?export\=download\&id\=1hmtbUQC5HuLb1KOMozNwCKFoAPa56rtx -O ./prediction_model.hdf5
